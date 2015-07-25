4cb4bb3fe2e210ccc22a5121145e9bbab67f79fb
Tyler Neely <t@jujit.su>
2015-07-25 16:23:47 -0700

    Add hooks for connect and getaddrinfo.  Nuke Cargo.lock as this will be a library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![crate_id = "discotech"]
 #![crate_type = "lib"]
-
+#![feature(libc)]
 #![feature(btree_range)]
 #![feature(collections_bound)]
 #![feature(convert)]
@@ -10,9 +10,33 @@
 extern crate log;
 extern crate rustc_serialize;
 extern crate zookeeper;
+#[macro_use]
+extern crate redhook;
+extern crate libc;
+
 
 pub use config::*;
 pub use serverset::*;
 
 pub mod config;
 pub mod serverset;
+
+use libc::types::os::common::bsd44::{addrinfo, socklen_t, sockaddr};
+use libc::{c_char, c_int, c_void};
+
+hook! {
+    fn connect(socket: c_int, address: *const sockaddr,
+               len: socklen_t) -> c_int => my_connect {
+        println!("HOOKING connect!!!!!!!!!!!!!!!!!");
+        unsafe { real!(connect)(socket, address, len) }
+    }
+}
+
+hook! {
+    fn getaddrinfo(node: *const c_char, service: *const c_char,
+                   hints: *const addrinfo,
+                   res: *const *const addrinfo) -> c_int => my_getaddrinfo {
+        println!("HOOKING getaddrinfo!!!!!!!!!!!!!!!!!");
+        unsafe { real!(getaddrinfo)(node, service, hints, res) }
+    }
+}
